d3c2738a5a3843f02251727b6317e89e88b5c414
Jhayla Young <jhayla.young@gmail.com>
2020-02-27 18:56:11 -0600

    Updates

---
+++ precache-manifest.42d3fa6e161253919167988dc87cac4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b3f0fd62b790aae8b37cd9f3fe5326",
+    "url": "/Google-Books/index.html"
+  },
+  {
+    "revision": "310d8d2f0c9986949213",
+    "url": "/Google-Books/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d3d16245d156389ec40b",
+    "url": "/Google-Books/static/js/2.189a16b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Google-Books/static/js/2.189a16b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310d8d2f0c9986949213",
+    "url": "/Google-Books/static/js/main.898da4a3.chunk.js"
+  },
+  {
+    "revision": "66764cad97950839c8e5",
+    "url": "/Google-Books/static/js/runtime-main.4b44d370.js"
+  }
+]);
